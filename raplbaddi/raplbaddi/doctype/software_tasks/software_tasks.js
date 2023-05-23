08ba6e608e015e0e30e7c238cb22827e9cc6e20b
Nishant Bhickta <nishantbhickta@gmail.com>
2023-05-23 06:48:55 +0000

    software tasks

---
+++ raplbaddi/raplbaddi/doctype/software_tasks/software_tasks.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Nishant Bhickta and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Software Tasks', {
+	// refresh: function(frm) {
+
+	// }
+});
