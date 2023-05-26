a6e529403700029e53b949b0ce67acef3e6c9c3b
Nishant Bhickta <nishantbhickta+code@gmail.com>
2023-05-26 06:33:35 +0000

    Production Entry

--- raplbaddi/production_rapl/doctype/production_entry/production_entry.js
+++ raplbaddi/production_rapl/doctype/production_entry/production_entry.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023, Nishant Bhickta and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Daily Production Entry', {
+frappe.ui.form.on('Production Entry', {
 	// refresh: function(frm) {
 
 	// }
--- raplbaddi/production_rapl/doctype/production_entry/test_production_entry.py
+++ raplbaddi/production_rapl/doctype/production_entry/test_production_entry.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestDailyProductionEntry(FrappeTestCase):
+class TestProductionEntry(FrappeTestCase):
 	pass
