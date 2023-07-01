127068161c986d9232d92138108ddbb247e8f6ef
Nishant Bhickta <nishantbhickta@gmail.com>
2023-07-01 08:13:21 +0000

    tasks module

---
+++ raplbaddi/tasks_rapl/doctype/task_user_role/task_user_role.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Nishant Bhickta and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Task User Role', {
+	// refresh: function(frm) {
+
+	// }
+});
