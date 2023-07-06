d3ef14de30b80787927022348c605b02fd8a5e89
Nishant Bhickta <nishantbhickta+code@gmail.com>
2023-07-06 17:11:51 +0530

    ?

--- raplbaddi/production_rapl/report/production_report/production_report.js
+++ raplbaddi/production_rapl/report/production_report/production_report.js
@@ -3,6 +3,36 @@
 /* eslint-disable */
 
 frappe.query_reports["Production Report"] = {
+	onload: function(report) {
+		arr = frappe.datetime.get_today().split("-")
+        arr.push("0" + (parseInt(arr.pop()) - 1))
+        yesterday = arr.join("-") 
+
+        report.page.add_inner_button(__("Today"), function() {
+            frappe.query_report.get_filter('start_date').set_value(frappe.datetime.get_today());
+            frappe.query_report.get_filter('end_date').set_value(frappe.datetime.get_today());
+        }, "Range");
+        report.page.add_inner_button(__("Yesterday"), function() {
+            frappe.query_report.get_filter('start_date').set_value(yesterday);
+            frappe.query_report.get_filter('end_date').set_value(yesterday);
+        }, "Range");
+        report.page.add_inner_button(__("Week"), function() {
+            frappe.query_report.get_filter('start_date').set_value(frappe.datetime.week_start());
+            frappe.query_report.get_filter('end_date').set_value(frappe.datetime.week_end());
+        }, "Range");
+		report.page.add_inner_button(__("Month"), function() {
+			frappe.query_report.get_filter('start_date').set_value(frappe.datetime.month_start());
+			frappe.query_report.get_filter('end_date').set_value(frappe.datetime.month_end());
+		}, "Range");
+		report.page.add_inner_button(__("Quarter"), function() {
+			frappe.query_report.get_filter('start_date').set_value(frappe.datetime.quarter_start());
+			frappe.query_report.get_filter('end_date').set_value(frappe.datetime.quarter_end());
+		}, "Range");
+        report.page.add_inner_button(__("Year"), function() {
+            frappe.query_report.get_filter('start_date').set_value(frappe.datetime.year_start());
+            frappe.query_report.get_filter('end_date').set_value(frappe.datetime.year_end());
+        }, "Range");
+    },
 	"filters": [
 		{
 			"fieldname": "item",
