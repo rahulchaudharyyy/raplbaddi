212ccfdb35df01ac6a31e4e63d39e26612707838
Nishant Bhickta <nishantbhickta+code@gmail.com>
2023-07-24 14:56:40 +0530

    Testing report

--- raplbaddi/testing_rapl/doctype/testing_report/testing_report.js
+++ raplbaddi/testing_rapl/doctype/testing_report/testing_report.js
@@ -2,8 +2,8 @@
 // For license information, please see license.txt
 frappe.ui.form.on('Testing Report', {
 	onload(frm) {
-		// frm.get_field("items").grid.cannot_add_rows = true;
-
+		frm.get_field("items").grid.cannot_add_rows = true;
+		frm.get_field("items").grid.cannot_delete_rows = true;
 		frm.set_query("department", function (doc) {
 			return {
 				filters: {
@@ -12,12 +12,25 @@ frappe.ui.form.on('Testing Report', {
 			};
 		});
 	},
+	fetch_parameters(frm) {
+		fetch_parameters(frm)
+	},
 	department(frm) {
 		let department_map = {
 			"Element : T - RAPL": "Element Unit",
 			"Tank: T - RAPL": "Tank Metal",
 			"Paintshop : T - RAPL": "Geyser Unit",
 		};
+
+		// frappe.db.get_value('Department', frm.doc.department, 'has_standard_testing').then(r => {
+		// 	let hasStandardTesting = r.message.has_standard_testing
+		// 	if (hasStandardTesting) {
+		// 		frm.add_child('items', { item_type: 'Item' })
+		// 	} else {
+		// 		frm.add_child('items', { item_type: 'Non Standard Testing Report Item' })
+		// 	}
+		// 	frm.refresh_field('items')
+		// })
 
 		let department = frm.doc.department;
 		let itemGroup = department_map[department] || "";
@@ -29,22 +42,29 @@ frappe.ui.form.on('Testing Report', {
 				}
 			};
 		});
-	},
-	fetch_parameters(frm) {
-		frappe.db.get_value('Department', frm.doc.department, 'abbriviation').then(r => {
-			let abb = r.message.abbriviation
-			frappe.db.get_list('Testing Parameters').then(
-				r => {
-					r.forEach(i => {
-						if (i.name.includes(abb) && frm.doc.item_name) {
-							frm.add_child('items', { parameter: i.name, item: frm.doc.item_name, item_code: frm.doc.item })
-						}
-					})
-					frm.refresh_field('items')
-					frm.set_value('item', '')
-					frm.refresh_field('item')
-				}
-			)
-		})
 	}
 });
+
+function fetch_parameters(frm) {
+	frappe.db.get_value('Department', frm.doc.department, 'abbriviation').then(r => {
+		let abb = r.message.abbriviation
+		console.log(abb);
+		frappe.db.get_list('Testing Parameters').then(
+			r => {
+				r.forEach(i => {
+					if (i.name.includes(abb)) {
+						console.log(i.name);
+						frm.add_child('items', {
+							parameter: i.name,
+							qty_tested: frm.doc.qty_tested,
+							item: frm.doc.item,
+							item_name: frm.doc.item_name,
+							item_type: 'Item'
+						})
+					}
+				})
+				frm.refresh_field('items')
+			}
+		)
+	})
+}
--- raplbaddi/testing_rapl/doctype/testing_report/testing_report.py
+++ raplbaddi/testing_rapl/doctype/testing_report/testing_report.py
@@ -1,8 +1,13 @@
 # Copyright (c) 2023, Nishant Bhickta and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+
 
 class TestingReport(Document):
 	pass
+    # def onload(self):
+    #     self.append("items", {
+    #         "item_type": "About Us Settings",
+    #     })
