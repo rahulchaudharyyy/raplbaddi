715720c9cb9060da1f96cf1d1c6d339daa790a49
Nishant Bhickta <nishantbhickta@gmail.com>
2023-09-03 17:15:06 +0530

    tally

---
+++ raplbaddi/datarapl/doctype/tally_data___1april_to_31_august_2023/test_tally_data___1april_to_31_august_2023.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Nishant Bhickta and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTallyData1Aprilto31August2023(FrappeTestCase):
+	pass
