a2a05e547ad5d87cee6bb499462794a07310a6dd
Nishant Bhickta <nishantbhickta@gmail.com>
2023-09-11 14:41:22 +0530

    billing rates and salesmanwise dn old too

---
+++ raplbaddi/stock_rapl/report/delivery_notes_all_salesmanwise/delivery_notes_all_salesmanwise.js
@@ -0,0 +1,62 @@
+// Copyright (c) 2023, Nishant Bhickta and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Delivery Notes All Salesmanwise"] = {
+	onload: function(report) {
+		// arr = frappe.datetime.get_today().split("-")
+        // arr.push("0" + (parseInt(arr.pop()) - 1))
+        // yesterday = arr.join("-") 
+
+        report.page.add_inner_button(__("Today"), function() {
+            frappe.query_report.get_filter('from_date').set_value(frappe.datetime.get_today());
+            frappe.query_report.get_filter('to_date').set_value(frappe.datetime.get_today());
+        }, "Range");
+        // report.page.add_inner_button(__("Yesterday"), function() {
+        //     frappe.query_report.get_filter('from_date').set_value(yesterday);
+        //     frappe.query_report.get_filter('to_date').set_value(yesterday);
+        // }, "Range");
+        report.page.add_inner_button(__("Week"), function() {
+            frappe.query_report.get_filter('from_date').set_value(frappe.datetime.week_start());
+            frappe.query_report.get_filter('to_date').set_value(frappe.datetime.week_end());
+        }, "Range");
+		report.page.add_inner_button(__("Month"), function() {
+			frappe.query_report.get_filter('from_date').set_value(frappe.datetime.month_start());
+			frappe.query_report.get_filter('to_date').set_value(frappe.datetime.month_end());
+		}, "Range");
+		report.page.add_inner_button(__("Quarter"), function() {
+			frappe.query_report.get_filter('from_date').set_value(frappe.datetime.quarter_start());
+			frappe.query_report.get_filter('to_date').set_value(frappe.datetime.quarter_end());
+		}, "Range");
+        report.page.add_inner_button(__("Year"), function() {
+            frappe.query_report.get_filter('from_date').set_value(frappe.datetime.year_start());
+            frappe.query_report.get_filter('to_date').set_value(frappe.datetime.year_end());
+        }, "Range");
+    },
+	"filters": [
+		{
+			"fieldname": "sales_person",
+			"label": __("Sales Person"),
+			"fieldtype": "Link",
+			"width": "80",
+			"options": "Customer Group",
+			"reqd": 1,
+		},
+		{
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"reqd": 1,
+			"default": frappe.datetime.year_start()
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"reqd": 1,
+			"default": frappe.datetime.year_end()
+		},
+	]
+};
---
+++ raplbaddi/stock_rapl/report/delivery_notes_all_salesmanwise/delivery_notes_all_salesmanwise.py
@@ -0,0 +1,93 @@
+# Copyright (c) 2023, Nishant Bhickta and contributors
+# For license information, please see license.txt
+import frappe
+
+
+def execute(filters=None):
+    columns, data = [], []
+    data = get_data(filters)
+    return get_columns(filters), data
+
+def get_data(filters):
+    query = f"""
+        SELECT
+            customer,
+            salesman,
+            SUM(net_sales) AS net_sales
+        FROM (
+            SELECT
+                DISTINCT
+                SUM(IF(td.transaction_type = 'Sales', td.quantity, -(td.quantity))) AS net_sales,
+                tp.customer AS customer,
+                cu.customer_group AS salesman,
+                td.date AS date
+            FROM
+                `tabTally April to August 2023` AS td
+                LEFT JOIN `tabTally Party Name` AS tp ON td.party_name = tp.name
+                LEFT JOIN `tabCustomer` AS cu ON tp.customer = cu.name
+            WHERE
+                td.particulars IS NOT NULL
+                AND particulars LIKE '%%Geyser%%'
+                AND td.date BETWEEN '2023-04-01' AND '2023-08-28'
+            GROUP BY
+                tp.customer
+
+            UNION
+
+            SELECT
+                DISTINCT
+                SUM(dn.total_qty) AS net_sales,
+                dn.customer_name AS customer,
+                cu.customer_group AS salesman,
+                dn.posting_date AS date
+            FROM
+                `tabDelivery Note` AS dn
+                LEFT JOIN `tabCustomer` AS cu ON dn.customer_name = cu.customer_name
+            WHERE
+                dn.posting_date >= '2023-08-29'
+                AND dn.docstatus = 1
+            GROUP BY
+                dn.customer_name
+            HAVING
+                MAX(dn.posting_date) >= '2023-08-29'
+        ) AS d
+        WHERE {get_conditions(filters)}
+        GROUP BY
+            d.customer
+    """
+
+    result = frappe.db.sql(query, as_dict=True)
+    return result
+
+
+def get_columns(filters):
+    columns = [
+        {
+            "label": "Customer",
+            "fieldname": "customer",
+            "fieldtype": "Link",
+            "options": "Customer",
+            "width": 250,
+        },
+        {
+            "label": "Net Sales",
+            "fieldname": "net_sales",
+            "fieldtype": "Int",
+            "width": 120,
+        }
+    ]
+    return columns
+
+
+def get_conditions(filters):
+    conditions = "1"
+    if filters and filters.get("from_date"):
+        from_date = filters.get("from_date")
+        conditions += f" AND d.date >= '{from_date}'"
+    if filters and filters.get("to_date"):
+        to_date = filters.get("to_date")
+        conditions += f" AND d.date <= '{to_date}'"
+    if filters and filters.get("sales_person"):
+        sales_person = filters.get("sales_person")
+        conditions += f" AND d.salesman = '{sales_person}'"
+    return conditions
