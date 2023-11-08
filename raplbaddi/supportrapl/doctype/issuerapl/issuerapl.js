d4eab54ff24d65dff96ac9f18a4b0ab609e88ab6
Nishant Bhickta <nishantbhickta@gmail.com>
2023-11-08 16:16:09 +0530

    Issue rapl SC KM Wise

--- raplbaddi/supportrapl/doctype/issuerapl/issuerapl.js
+++ raplbaddi/supportrapl/doctype/issuerapl/issuerapl.js
@@ -1,8 +1,30 @@
 // Copyright (c) 2023, Nishant Bhickta and contributors
 // For license information, please see license.txt
-
 frappe.ui.form.on('IssueRapl', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function (frm) {
+		frm.add_custom_button(__('Select SC'), function () {
+			frm.call({
+				method: 'get_addresses',
+				doc: frm.doc,
+				callback: function (response) {
+					if (response.message) {
+						var options = response.message;
+						frappe.prompt({
+							label: __('Select an Address'),
+							fieldname: 'selected_address',
+							fieldtype: 'Select',
+							options: options,
+							reqd: 1,
+						},
+							(values) => {
+								value = values.selected_address.split(':')
+								frm.set_value('service_centre', value[1])
+								frm.set_value('kilometer', value[0])
+							});
+					}
+				}
+			});
+		});
+	}
 });
+
--- raplbaddi/supportrapl/doctype/issuerapl/issuerapl.py
+++ raplbaddi/supportrapl/doctype/issuerapl/issuerapl.py
@@ -4,18 +4,38 @@
 import frappe
 from frappe.model.document import Document
 import requests
+import googlemaps
 
 class IssueRapl(Document):
-    def validate(self):
-        api_key = 'AIzaSyAO5NhknKUS-KUOTjs48mRC9PBAWi2hB70'
-        customer_address = self.customer_address + self.district + self.state + self.pin_code
-        service_centre = frappe.get_doc('Service Centre', self.service_centre)
-        service_centre_address = service_centre.address + service_centre.district + service_centre.pincode + service_centre.state + service_centre.country
-        url = f'https://maps.googleapis.com/maps/api/distancematrix/json?origins={service_centre_address}&destinations={customer_address}&key={api_key}'
-        response = requests.get(url=url)
-        if response.status_code == 200:
-            data = response.json()
-            self.kilometer = (data.get('rows')[0].get('elements')[0].get('distance').get('value')/1000)
-        else:
-            frappe.throw(f"Error: {response.status_code} - {response.text}")
-    
+	def get_sc_address(self):
+		return frappe.get_all('Service Centre', ['state', 'district', 'pincode', 'address', 'name'])
+	@frappe.whitelist()
+	def get_addresses(self):
+		gmaps = googlemaps.Client(key='AIzaSyAO5NhknKUS-KUOTjs48mRC9PBAWi2hB70	')
+		sc_addresses = []
+		for sc in self.get_sc_address():
+			sc_address = ''
+			for sc in sc.values():
+				sc_address = sc
+			sc_addresses.append(sc_address)
+		chunk_size = 25
+		split_sc_addresses = [sc_addresses[i:i + chunk_size] for i in range(0, len(sc_addresses), chunk_size)]
+		dms = []
+		matrix = {}
+		customer_address = " ".join([self.state, self.district, self.pin_code, self.customer_address])
+		for sc_address in split_sc_addresses:
+			distance_matrix = gmaps.distance_matrix(
+				origins=sc_address, destinations=customer_address)
+			dms.append(distance_matrix)
+		for dm in dms:
+			if 'rows' in dm and dm['rows']:
+				for i, row in enumerate(dm['rows']):
+					if 'elements' in row and row['elements']:
+						for j, element in enumerate(row['elements']):
+							origin_address = sc_addresses[i]
+							if 'status' in element and element['status'] == 'OK':
+								distance = int(element['distance']['value']/1000)
+								matrix[origin_address] = distance
+			options = [{'label': str(value) + ': ' + key, 'value': str(value) + ':' + key, 'km': value} for key, value in matrix.items()]
+			options_sorted = sorted(options, key=lambda x: x['km'], reverse=False)
+			return options_sorted
