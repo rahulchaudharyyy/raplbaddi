9d1f430274ca1b11f6c87caa1af925fd47cfce0f
Rahul dev <diwaniwalrahul9@gmail.com>
2024-01-06 09:29:08 +0530

    Little changes in payements_service_center.py

--- raplbaddi/supportrapl/report/payments_service_center/payments_service_center.py
+++ raplbaddi/supportrapl/report/payments_service_center/payments_service_center.py
@@ -30,33 +30,46 @@ class PayementReport:
         
 	def get_columns(self):
 		columns = [
-		{'fieldname': 'complaint_no','label': ('Complaint No.'),'fieldtype': 'Link','options':'IssueRapl'},
-		{'fieldname': 'Date','label': ('Date'),'fieldtype': 'Date'},
-		{'fieldname': 'Service Center','label': ('Service Center'),'fieldtype': 'Link','options':'Service Centre'},
-		{'fieldname': 'Bank','label': ('Bank Name'),'fieldtype': 'Data'},
-		{'fieldname': 'Account No.','label': ('Account No.'),'fieldtype': 'Data'},
-		{'fieldname': 'Bank IFSC','label': ('IFSC Code'),'fieldtype': 'Data'},
-		{'fieldname': 'UPI Id','label': ('UPI Id'),'fieldtype': 'Data'},
-		{'fieldname': 'KMs','label': ('Kilometers'),'fieldtype': 'Data'},
-		{'fieldname': 'Amount','label': ('Amount'),'fieldtype': 'Data'}
-
+		{'fieldname': 'complaint_no','label': ('Complaint No.'),'fieldtype': 'Link','options':'IssueRapl','width':120},
+		{'fieldname': 'Date','label': ('Date'),'fieldtype': 'Date','width':120},
+		{'fieldname': 'Service Center','label': ('Service Center'),'fieldtype': 'Link','options':'Service Centre','width':180},
+		{'fieldname': 'Bank','label': ('Bank Name'),'fieldtype': 'Data','width':150},
+		{'fieldname': 'Account No.','label': ('Account No.'),'fieldtype': 'Data','width':130},
+		{'fieldname': 'Bank IFSC','label': ('IFSC Code'),'fieldtype': 'Data','width':130},
+		{'fieldname': 'UPI Id','label': ('UPI Id'),'fieldtype': 'Data','width':120},
+		{'fieldname': 'KMs','label': ('KMs'),'fieldtype': 'Data','width':70},
+		{'fieldname': 'Amount','label': ('Amount'),'fieldtype': 'Data','width':90}
 		]
 		return columns
+    
     	
 	def get_filtered_data(self):
 		start_date = self.filters.get('start_date')
 		end_date = self.filters.get('end_date')
 		service_centre = self.filters.get('Service_Centre')
 		self.get_data()
-		filterd_data = []
+		self.filtered_data = []
 		for i in self.data:
 			if (not service_centre or i['Service Center'] == service_centre)and(not start_date or i['Date'] >= start_date) and (not end_date or i['Date'] <= end_date):
-				filterd_data.append(i)
-		return filterd_data
+				self.filtered_data.append(i)
+		return self.filtered_data
 
+	def get_count(self):
+		return len(self.filtered_data)
+
+
+	def get_msg(self):
+		count = self.get_count()
+		# amount = self.get_total_amount()
+		# sales_person = self.filters.sales_person
+		return f"""
+		<div style="display: flex; justify-content: space-between;">
+			<h3>Complaints : {count}</h3>
+			
+		</div>"""
 
 
 def execute(filters=None):
 	payment = PayementReport(filters)
 	columns, data = [], []
-	return payment.get_columns(), payment.get_filtered_data()
+	return payment.get_columns(), payment.get_filtered_data(),payment.get_msg()
