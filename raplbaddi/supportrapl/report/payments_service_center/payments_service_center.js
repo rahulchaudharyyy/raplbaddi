575a12ca0185698895c851f3c3eb7bd7d2db1864
Rahul dev <diwaniwalrahul9@gmail.com>
2024-01-06 12:27:51 +0530

    Updated Payments_service_center report

--- raplbaddi/supportrapl/report/payments_service_center/payments_service_center.js
+++ raplbaddi/supportrapl/report/payments_service_center/payments_service_center.js
@@ -2,6 +2,36 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Payments Service Center"] = {
+	onload: function(report) {
+		// arr = frappe.datetime.get_today().split("-")
+        // arr.push("0" + (parseInt(arr.pop()) - 1))
+        // yesterday = arr.join("-") 
+
+        report.page.add_inner_button(__("Today"), function() {
+            frappe.query_report.get_filter('start_date').set_value(frappe.datetime.get_today());
+            frappe.query_report.get_filter('end_date').set_value(frappe.datetime.get_today());
+        }, "Range");
+        // report.page.add_inner_button(__("Yesterday"), function() {
+        //     frappe.query_report.get_filter('start_date').set_value(yesterday);
+        //     frappe.query_report.get_filter('end_date').set_value(yesterday);
+        // }, "Range");
+        report.page.add_inner_button(__("Week"), function() {
+            frappe.query_report.get_filter('start_date').set_value(frappe.datetime.week_start());
+            frappe.query_report.get_filter('end_date').set_value(frappe.datetime.week_end());
+        }, "Range");
+		report.page.add_inner_button(__("Month"), function() {
+			frappe.query_report.get_filter('start_date').set_value(frappe.datetime.month_start());
+			frappe.query_report.get_filter('end_date').set_value(frappe.datetime.month_end());
+		}, "Range");
+		report.page.add_inner_button(__("Quarter"), function() {
+			frappe.query_report.get_filter('start_date').set_value(frappe.datetime.quarter_start());
+			frappe.query_report.get_filter('end_date').set_value(frappe.datetime.quarter_end());
+		}, "Range");
+        report.page.add_inner_button(__("Year"), function() {
+            frappe.query_report.get_filter('start_date').set_value(frappe.datetime.year_start());
+            frappe.query_report.get_filter('end_date').set_value(frappe.datetime.year_end());
+        }, "Range");
+    },
 	"filters": [
 		{
 			'fieldname':'Service_Centre',
@@ -18,7 +48,12 @@ frappe.query_reports["Payments Service Center"] = {
 			'fieldname': 'end_date',
 			'label': __('End Date'),
 			'fieldtype': 'Date'
-		}	
+		},
+		{
+			'fieldname': 'is_paid',
+			'label': __('Paid'),
+			'fieldtype': 'Check'
+		}
 
 	]
 };
