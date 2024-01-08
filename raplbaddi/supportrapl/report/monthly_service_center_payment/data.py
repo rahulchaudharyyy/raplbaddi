9b2b4c62ff234db49165633cac89276dcd73f8f4
bhickta <61617746+bhickta@users.noreply.github.com>
2024-01-08 14:19:44 +0530

    Rahul dev (#34)
    * working monthly report v0
    
    * Created new report Monthly Service Center Payments
    
    ---------
    
    Co-authored-by: Rahul dev <diwaniwalrahul9@gmail.com>

---
+++ raplbaddi/supportrapl/report/monthly_service_center_payment/data.py
@@ -0,0 +1,52 @@
+def get_query(filters, conditions):
+    group_by_query = f"""
+        select 
+            COUNT(i.name) as 'count',
+            CASE WHEN i.custom_creation_date THEN i.custom_creation_date ELSE i.creation END as 'date',
+            j.service_centre_name as 'service_centre',
+            j.bank_name as 'bank',
+            j.bank_account_no as 'account_no',
+            j.ifsc_code as 'ifsc',
+            j.upi_id as 'upi',
+            sum(i.amount) as 'amount',
+            i.payment_done,
+            i.service_delivered,
+            i.customer_confirmation
+        from tabIssueRapl as i
+            left Join
+            `tabService Centre` as j on i.service_centre = j.service_centre_name
+        where
+            {conditions}
+        group by
+            i.service_centre
+        order by
+            amount DESC, count DESC
+        """
+    ungrouped_query = f"""
+        select 
+            1 as 'count',
+            i.name as complaint_no,
+            CASE WHEN i.custom_creation_date THEN i.custom_creation_date ELSE i.creation END as 'date',
+            j.service_centre_name as 'service_centre',
+            j.bank_name as 'bank',
+            j.bank_account_no as 'account_no',
+            j.ifsc_code as 'ifsc',
+            j.upi_id as 'upi',
+            i.amount as 'amount',
+            i.payment_done,
+            i.service_delivered,
+            i.customer_confirmation,
+            i.kilometer as 'kms'
+
+        from tabIssueRapl as i
+            left Join
+            `tabService Centre` as j on i.service_centre = j.service_centre_name
+        where
+            {conditions}
+        order by
+            amount DESC, count DESC
+        """
+        
+    query = group_by_query if filters.group_by_sc else ungrouped_query
+    print(query)
+    return query
