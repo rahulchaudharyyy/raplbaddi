75b09fa5e4f4d3191b2fc7d930ef65cb639c055f
bhickta <61617746+bhickta@users.noreply.github.com>
2024-01-09 12:41:47 +0530

    Issuerapljs (#37)
    * working js
    :
    
    * issue rapl working
    
    ---------
    
    Co-authored-by: Nishant Bhickta <nishantbhickta@gmail.com>

--- raplbaddi/supportrapl/doctype/issuerapl/issuerapl.js
+++ raplbaddi/supportrapl/doctype/issuerapl/issuerapl.js
@@ -1,94 +1,95 @@
 class IssueRaplForm {
-	constructor(frm) {
-		this.frm = frm;
-		this.placeInputId = '#places-input'
-		this.placeInput = $(this.placeInputId)
-	}
+    constructor(frm) {
+        this.frm = frm;
+        this.placesInputId = 'places-input';
+    }
 
-	setup() {
-		$("#form-tabs").append(`<input type='text' id='${this.placeInputId}' style='width: 700px; height: 40px;'>`);
-		this.getGoogleApiKey();
-		this.addButtons()
-	}
+    setup() {
+        this.getGoogleApiKey();
+        this.addElements();
+    }
 
-	getGoogleApiKey() {
-		frappe.db.get_single_value('Google Settings', 'api_key')
-			.then(apiKey => {
-				if (apiKey) {
-					this.apiKey = apiKey;
-					this.loadScript();
-				} else {
-					console.error("Google API key is not set in 'Google Settings'.");
-				}
-			});
-	}
+    refresh() {
+        this.addButtons();
+    }
 
-	loadScript() {
-		let script = document.createElement('script');
-		script.src = `https://maps.googleapis.com/maps/api/js?key=${this.apiKey}&libraries=places`;
-		script.async = true;
-		script.defer = true;
+    addElements() {
+        $("#form-tabs").append(`<input type='text' id='${this.placesInputId}' style='width: 700px; height: 40px;'>`);
+    }
 
-		script.addEventListener('load', () => this.handleScriptLoad());
-	}
-	handleScriptLoad() {
-		console.log('In script')
-		frappe.google = new google.maps.places.Autocomplete(document.getElementById(this.placeInput), { componentRestrictions: { country: "in" } });
-		frappe.google.addListener('place_changed', () => this.handlePlaceChanged());
-	}
+    getGoogleApiKey() {
+        frappe.db.get_single_value('Google Settings', 'api_key')
+            .then(apiKey => this.loadScript(apiKey));
+    }
 
-	handlePlaceChanged() {
-		let frm = this.frm;
-		frm.selectedPlace = frappe.google.getPlace();
-		frm.set_value('customer_address', frm.selectedPlace.formatted_address);
-	}
+    loadScript(apiKey) {
+        let script = document.createElement('script');
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`;
+        script.async = true;
+        script.defer = true;
 
-	addButtons() {
-		this.frm.add_custom_button(__('Select SC'), () => {
-			this.frm.call({
-				method: 'get_addresses',
-				doc: this.frm.doc,
-				callback: response => this.handleAddressResponse(response)
-			});
-		});
-	}
+        script.addEventListener('load', () => this.handleScriptLoad());
 
-	handleAddressResponse(response) {
-		if (response.message) {
-			let options = response.message;
-			this.showAddressPrompt(options);
-		}
-	}
+        document.head.appendChild(script);
+    }
 
-	showAddressPrompt(options) {
-		frappe.prompt({
-			label: __('Select an Address'),
-			fieldname: 'selected_address',
-			fieldtype: 'Select',
-			options: options,
-			reqd: 1,
-		}, values => this.setServiceCentreValue(values.selected_address));
-	}
+    handleScriptLoad() {
+        frappe.google = new google.maps.places.Autocomplete(document.getElementById(this.placesInputId), { componentRestrictions: { country: "in" } });
+        frappe.google.addListener('place_changed', () => this.handlePlaceChanged());
+    }
 
-	setServiceCentreValue(selectedAddress) {
-		let value = selectedAddress.split(':');
-		this.frm.set_value('service_centre', value[0]);
+    handlePlaceChanged() {
+        let frm = this.frm;
+        frm.selectedPlace = frappe.google.getPlace();
+        frm.set_value('customer_address', frm.selectedPlace.formatted_address);
+    }
 
-		this.frm.call({
-			method: 'set_kilometers',
-			doc: this.frm.doc,
-			args: {
-				service_centre: value[0],
-				aerial: value[1]
-			},
-			callback: () => {
-				console.log(this.frm.doc.kilometer);
-				this.frm.save()
-			}
-		});
-	}
+    addButtons() {
+        this.frm.add_custom_button(__('Select SC'), () => {
+            this.frm.call({
+                method: 'get_addresses',
+                doc: this.frm.doc,
+                callback: response => this.handleAddressResponse(response)
+            });
+        });
+    }
+
+    handleAddressResponse(response) {
+        if (response.message) {
+            let options = response.message;
+            this.showAddressPrompt(options);
+        }
+    }
+
+    showAddressPrompt(options) {
+        frappe.prompt({
+            label: __('Select an Address'),
+            fieldname: 'selected_address',
+            fieldtype: 'Select',
+            options: options,
+            reqd: 1,
+        }, values => this.setServiceCentreValue(values.selected_address));
+    }
+
+    setServiceCentreValue(selectedAddress) {
+        let value = selectedAddress.split(':');
+        this.frm.set_value('service_centre', value[0]);
+
+        this.frm.call({
+            method: 'set_kilometers',
+            doc: this.frm.doc,
+            args: {
+                service_centre: value[0],
+                aerial: value[1]
+            },
+            callback: () => {
+                this.frm.save();
+            }
+        });
+    }
 }
 
 frappe.ui.form.on('IssueRapl', {
-	refresh: frm => new IssueRaplForm(frm).setup(),
+    setup: frm => new IssueRaplForm(frm).setup(),
+    refresh: frm => new IssueRaplForm(frm).refresh(),
 });
