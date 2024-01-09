59e614b85f50c885363e245af5f5dd4c1959c8e7
Nishant Bhickta <nishantbhickta@gmail.com>
2024-01-09 11:01:51 +0530

    service distance logic change

--- raplbaddi/supportrapl/doctype/issuerapl/issuerapl_list.js
+++ raplbaddi/supportrapl/doctype/issuerapl/issuerapl_list.js
@@ -1,12 +1,12 @@
-frappe.listview_settings['IssueRapl'] = {
-    onload: function (listview) {
-        listview.page.add_menu_item(__("Calculate Amounts"), function () {
-            frappe.call({
-                method: 'raplbaddi.supportrapl.doctype.issuerapl.issuerapl.set_rates',
-                callback: function (response) {
-                    frappe.throw('Amounts has been calcualted')
-                }
-            })
-        });
-    }
-}
+// frappe.listview_settings['IssueRapl'] = {
+//     onload: function (listview) {
+//         listview.page.add_menu_item(__("Calculate Amounts"), function () {
+//             frappe.call({
+//                 method: 'raplbaddi.supportrapl.doctype.issuerapl.issuerapl.set_rates',
+//                 callback: function (response) {
+//                     frappe.throw('Amounts has been calcualted')
+//                 }
+//             })
+//         });
+//     }
+// }
