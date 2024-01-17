4b2d0398cc0da649ed8fa783bcd2e72811378485
Rahul dev <diwaniwalrahul9@gmail.com>
2024-01-17 11:33:09 +0530

    Created New Report daily issue registered

---
+++ raplbaddi/supportrapl/report/daily_issues_registered/daily_issues_registered.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2024, Nishant Bhickta and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Daily Issues Registered"] = {
+	onload : function(report){
+		report.page.add_inner_button(__("Today"), function () {
+		frappe.query_report.get_filter('start_date').set_value(frappe.datetime.get_today());
+	});
+
+	},
+
+	"filters": [
+		{
+			'fieldname': 'start_date',
+			'label': __('Start Date'),
+			'fieldtype': 'Date',
+			'reqd': 1,
+			'hidden': 1
+		}
+	]
+};
